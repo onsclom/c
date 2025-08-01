9d5cff571687f86a03c727d57b8ca20dff6bb062
Austin Merrick <austinmerrick@onsclom.net>
2025-07-31 23:13:18 -0700

    .

--- game/build-and-run.sh
+++ game/build-and-run.sh
@@ -1,3 +1,4 @@
 cc game.c raylib/libraylib.a -Iraylib \
 -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL \
--o game && ./game
+-o game \
+&& ./game
--- game/game.c
+++ game/game.c
@@ -1,6 +1,12 @@
 #include "raylib/raylib.h"
+#include <stddef.h>
 
-#include <stdio.h>
+#define DEFINE_LIST(type, name, max_count)                                     \
+  typedef struct {                                                             \
+    type items[max_count];                                                     \
+    int count;                                                                 \
+    int capacity;                                                              \
+  } name
 
 typedef struct {
   Rectangle rect;
@@ -8,17 +14,28 @@ typedef struct {
 } Player;
 
 typedef struct {
+  // width and height always 1
+  int x;
+  int y;
+} Tile;
+
+#define TILE_LIST_CAPACITY 1000
+DEFINE_LIST(Tile, TileList, TILE_LIST_CAPACITY);
+
+typedef struct {
   Player player;
   Camera2D camera;
   float zoomFactor;
+  TileList tiles;
 } Game;
 
-int main(void) {
-  const int screenWidth = 400;
-  const int screenHeight = 400;
+TileList tiles = {0};
 
+int main(void) {
+  const int initScreenWidth = 600;
+  const int initScreenHeight = 600;
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(initScreenWidth, initScreenHeight, "platformer");
   const Vector2 dpi = GetWindowScaleDPI();
   const int fontSize = 20 * dpi.x;
   Font fontTtf = LoadFontEx("resources/IBMPlexMono.ttf", fontSize, 0, 250);
@@ -26,21 +43,22 @@ int main(void) {
   Game game = {
       .player =
           {
-              .rect = {0, 0, .8, 1.4},
+              .rect = {0, 0, .8, 1.2},
               .dy = 0,
           },
       .camera =
           {
               .offset =
                   {
-                      screenWidth / 2.0f,
-                      screenHeight / 2.0f,
+                      initScreenWidth / 2.0f,
+                      initScreenHeight / 2.0f,
                   },
               .target = {0, 0},
               .rotation = 0.0f,
               .zoom = 1.0f,
           },
       .zoomFactor = 1.0f,
+      .tiles = {0, .capacity = TILE_LIST_CAPACITY},
   };
 
   double lastFrameTime = GetTime();
@@ -49,9 +67,6 @@ int main(void) {
     const double currentFrameTime = GetTime();
     const double deltaTime = currentFrameTime - lastFrameTime;
     lastFrameTime = currentFrameTime;
-
-    const float defaultGameWidth = 10;
-    const float defaultGameHeight = 10;
 
     {
       float speed = 10;
@@ -75,37 +90,47 @@ int main(void) {
       if (IsKeyDown(KEY_MINUS)) {
         game.zoomFactor -= zoomSpeed * deltaTime;
       }
-      game.camera.zoom =
-          (float)screenWidth / defaultGameWidth * game.zoomFactor;
+
+      {
+        const float defaultGameWidth = 20;
+        const int screenWidth = GetScreenWidth();
+        const int screenHeight = GetScreenHeight();
+        game.camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
+        game.camera.zoom =
+            (float)screenWidth / defaultGameWidth * game.zoomFactor;
+        game.camera.target = (Vector2){0, 0};
+      }
     }
 
     BeginDrawing();
-
     ClearBackground(RAYWHITE);
-
     BeginMode2D(game.camera);
     {
       DrawRectanglePro(game.player.rect,
                        (Vector2){game.player.rect.width / 2.0f,
                                  game.player.rect.height / 2.0f},
                        0.0f, BLUE);
-
       DrawRectanglePro((Rectangle){-100, 100, game.player.rect.width,
                                    game.player.rect.height},
                        (Vector2){game.player.rect.width / 2.0f,
                                  game.player.rect.height / 2.0f},
                        0.0f, Fade(GRAY, 0.5f));
     }
-    EndMode2D();
 
+    // get mouse position in world coordinates
+    Vector2 mousePosition = GetMousePosition();
+    Vector2 mouseInCamera = GetScreenToWorld2D(mousePosition, game.camera);
+    // draw a circle at the mouse position
+    DrawCircleV(mouseInCamera, 1, RED);
+
+    EndMode2D();
     DrawTextEx(fontTtf, "hello world\nwow", (Vector2){20.0f, 100.0f},
                (float)fontTtf.baseSize / dpi.x, 2, GRAY);
-
     DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, DARKGRAY);
-    DrawText(TextFormat("ZOOM: %.2f", game.camera.zoom), 10, 40, 20, DARKGRAY);
+    DrawText(TextFormat("ZOOM: %.2f", game.zoomFactor), 10, 40, 20, DARKGRAY);
+
     EndDrawing();
   }
-
   CloseWindow();
 
   return 0;
